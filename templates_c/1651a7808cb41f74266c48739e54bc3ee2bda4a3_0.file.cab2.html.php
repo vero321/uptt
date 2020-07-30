965abb39591d2a918dea11c5c56fce7b3e3f951a
palencia575 <palencia575@gmail.com>
2020-07-30 10:53:04 -0400

    pruebas de tema

---
+++ templates_c/1651a7808cb41f74266c48739e54bc3ee2bda4a3_0.file.cab2.html.php
@@ -0,0 +1,54 @@
+<?php
+/* Smarty version 3.1.30, created on 2020-07-30 10:48:10
+  from "/home/palencia/public_html/proyecto4/uptt/modulos/navegacion/cab2.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5f22ddaa2d7a84_96797987',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '1651a7808cb41f74266c48739e54bc3ee2bda4a3' => 
+    array (
+      0 => '/home/palencia/public_html/proyecto4/uptt/modulos/navegacion/cab2.html',
+      1 => 1596053845,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f22ddaa2d7a84_96797987 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<!--Comienzo de la Cabecera-->
+<head>
+  <meta charset="utf-8"/>
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <!-- llama los estilos de DataTablet -->
+  <link href="../../vendor/DataTables/datatables.min.css" rel="stylesheet" type="text/css">
+  <link href="../../vendor/DataTables/css/dataTables.bootstrap4.css" rel="stylesheet"  type="text/css">
+  <link href="../../estilos/sb-admin-2.css" rel="stylesheet">
+  <link href="../../estilos/personal2.css" rel="stylesheet">
+  <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+   <!--Estilo de los iconos-->
+  <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+
+
+
+
+
+  <link href="../../vendor/DataTables/buttons/buttons.dataTables.min.css" rel="stylesheet" type="text/css">
+  <link href="../../vendor/DataTables/buttons/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
+
+
+
+  <title>Repositorio</title>
+</head>
+<body id="page-top" style=" background-color: #fff !important;"><?php }
+}
---
+++ templates_c/4f17409ac231e6c97fb2ba170b82b16f1af88d78_0.file.pie_interno.html.php
@@ -0,0 +1,118 @@
+<?php
+/* Smarty version 3.1.30, created on 2020-07-30 10:47:47
+  from "/home/palencia/public_html/proyecto4/uptt/modulos/pie/pie_interno.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5f22dd93bf6ab8_35056316',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '4f17409ac231e6c97fb2ba170b82b16f1af88d78' => 
+    array (
+      0 => '/home/palencia/public_html/proyecto4/uptt/modulos/pie/pie_interno.html',
+      1 => 1596115851,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f22dd93bf6ab8_35056316 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+    <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+  
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">¿Preparado para salir?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+          <a class="btn btn-primary" href="../login/proc_exit.php">Cerrar sesión</a>
+        </div>
+      </div>
+    </div>
+  </div>
+<!--Comienzo del Pie de Página-->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Realizado por Verónica Osuna, Karla Quintero, José Palencia y Brando Hernández</span>
+          </div>
+        </div>
+      </footer>
+<!-- End of Footer -->
+ 
+
+
+
+    </footer>
+
+
+
+<<<<<<< HEAD
+    <!--script src="../../vendor/jquery/jquery-3.3.1.min.js"><?php echo '</script'; ?>
+-->
+
+=======
+    <?php echo '<script'; ?>
+ src="../../vendor/jquery/jquery-3.3.1.min.js"><?php echo '</script'; ?>
+>
+>>>>>>> 8f8489723f1a4a68d3cce37b506d4facfadd5a29
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/datatables.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../../vendor/bootstrap/js/bootstrap.min.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../config/main.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="vendor/jquery-easing/jquery.easing.min.js"><?php echo '</script'; ?>
+>
+
+    <!--para que funcionen los de exportar data table-->
+    
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/jquery/dataTables.buttons.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/jquery/jszip.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/jquery/pdfmake.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/jquery/vfs_fonts.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/jquery/buttons.html5.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="../../vendor/DataTables/jquery/buttons.print.min.js" type="text/javascript"><?php echo '</script'; ?>
+>
+
+<!--Final del Pie de Página-->
+
+        
+<?php echo (($tmp = @$_smarty_tpl->tpl_vars['pie']->value)===null||$tmp==='' ? '' : $tmp);?>
+
+</body>
+</html>
+<?php }
+}
